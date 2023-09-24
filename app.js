ce55c010cca9e0ec110dd05166873d4df4d4615a
Gabrielle <118292803+gabszuba@users.noreply.github.com>
2023-09-23 21:08:19 -0300

    Finish functionalities

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const gameBoard = (() => {
-    const board = ["", "", "", "", "", "", "", "", ""];
+    const gameBoardElement = document.getElementById("game-board");
+    
+    let board = ["", "", "", "", "", "", "", "", ""]; // Use let instead of const
 
     const renderBoard = () => {
-        const gameBoardElement = document.getElementById("game-board");
         board.forEach((cell) => {
             const cellElement = document.createElement("div");
             cellElement.textContent = cell;
@@ -12,9 +13,18 @@ const gameBoard = (() => {
     };
 
     const getBoard = () => board;
-    
+
+    const clearBoard = () => { 
+        const cellElements = gameBoardElement.querySelectorAll(".cell");
+        cellElements.forEach((cell, index) => {
+            board[index] = ""; 
+            cell.textContent = "";
+        });
+    }
+      
     return {
-        renderBoard,
+        renderBoard, 
+        clearBoard,
         getBoard   
     };
 })();
@@ -29,31 +39,81 @@ const Player = (name, marker) => {
     }
 };
 
+function checkForWin(board, marker) {
+    const winCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]            
+    ];
+
+    for (const combination of winCombinations) {
+        const [a, b, c] = combination;
+        if (board[a] === marker && board[b] === marker && board[c] === marker) {
+            return true; 
+        }
+    }
+    return false; 
+}
+
+function displayWinner(winner) {
+    const winnerText = document.createElement("p");
+    winnerText.textContent = `${winner.getName()} wins!`;
+    document.body.insertBefore(winnerText, document.getElementById("game-board"));
+}
+
+
 const gameController = (() => {
     const startButton = document.getElementById("start-button");
     const board = gameBoard.getBoard();
-    
+    let winner = false; 
+
     let player1 = Player("Player 1", "X");
-    let player2 = Player("Player2", "O");
-    currentPlayer = player1; 
+    let player2 = Player("Player 2", "O");
+
+    let currentPlayer = player1;
 
     startButton.addEventListener("click", () => {
-        gameBoard.renderBoard(); 
-        startButton.disabled = true; 
+        gameBoard.renderBoard();
+        startButton.style.display = "none";
 
         const cells = document.querySelectorAll(".cell");
         cells.forEach((cell, index) => {
             cell.addEventListener("click", () => {
-                if (cell.textContent === "") {
+                if (!winner && cell.textContent === "") {
                     board[index] = cell.textContent = currentPlayer.getMarker();
                     console.log(board);
-                    currentPlayer = currentPlayer === player1 ? player2 : player1;
+                    winner = checkForWin(board, currentPlayer.getMarker());
+                    if (winner) {
+                        displayWinner(currentPlayer);
+                        createRestartButton();
+                    } else {
+                        currentPlayer = currentPlayer === player1 ? player2 : player1;
+                    }
                 }
             });
         });
     });
+
+    function createRestartButton() {
+        const restartButton = document.createElement("button");
+        restartButton.textContent = "Restart Game";
+        restartButton.id = "restart-button";
+        restartButton.addEventListener("click", () => {
+            restartGame();
+        });
+        document.body.insertBefore(restartButton, document.getElementById("game-board"));
+    }
+
+    const restartGame = () => {
+        winner = false; 
+        gameBoard.clearBoard();
+
+        const restartButton = document.getElementById("restart-button");
+        if (restartButton) {
+            restartButton.remove();
+        }
+
+        const winnerText = document.querySelector("p");
+        winnerText.remove();
+    };
 })();
-
-
-
-
